763b35fd702bd567dcecc3530b527f39e66458fd
faizafrin <faizasworld2022@gmail.com>
2023-05-07 00:33:26 +0530

    firstcommit

---
+++ src/components/Header.js
@@ -0,0 +1,97 @@
+import React, { useContext } from 'react'
+import Avatar from '@mui/material/Avatar';
+import "./header.css"
+import { LoginContext } from './ContextProvider/Context';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { useNavigate , NavLink } from "react-router-dom"
+
+const Header = () => {
+
+    const { logindata} = useContext(LoginContext);
+
+    const history = useNavigate();
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const navigate = useNavigate();
+
+    const logoutuser = async () => {
+       
+
+
+            localStorage.clear()
+            navigate('/')
+        
+
+     
+    }
+
+    const goDash = () => {
+        history("/dash")
+    }
+
+    const goError = () => {
+        history("*")
+    }
+
+    return (
+        <>
+            <header>
+                <nav>
+                    
+                <NavLink to="/"><h1>Log In System</h1></NavLink>
+                    <div className="avtar">
+                        {
+                            logindata.ValidUserOne ? <Avatar style={{ background: "salmon", fontWeight: "bold", textTransform: "capitalize" }} onClick={handleClick}>{logindata.ValidUserOne.fname[0].toUpperCase()}</Avatar> :
+                                <Avatar style={{ background: "blue" }} onClick={handleClick} />
+                        }
+
+                    </div>
+
+                    <Menu
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                            'aria-labelledby': 'basic-button',
+                        }}
+                    >
+                        {
+                            logindata.ValidUserOne ? (
+                                <>
+                                    <MenuItem onClick={() => {
+                                        goDash()
+                                        handleClose()
+                                    }}>Profile</MenuItem>
+                                    <MenuItem onClick={() => {
+                                        logoutuser()
+                                        handleClose()
+                                    }}>Logout</MenuItem>
+                                </>
+                            ) : (
+                                <>
+                                    <MenuItem onClick={() => {
+                                        goError()
+                                        handleClose()
+                                    }}>Profile</MenuItem>
+                                </>
+                            )
+                        }
+
+                    </Menu>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Header
